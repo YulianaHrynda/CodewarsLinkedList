68c5c57e75efc3d4ee03f7b7a0c166b8f1c447e7
YulianaHrynda <146963885+YulianaHrynda@users.noreply.github.com>
2024-04-02 23:30:08 +0300

    Create recursive_reverse.py

---
+++ Linked Lists - Recursive Reverse/recursive_reverse.py
@@ -0,0 +1,15 @@
+class Node(object):
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+def reverse(head):
+    if head is None or head.next is None:
+        return head
+
+    reversed_tail = reverse(head.next)
+
+    head.next.next = head
+    head.next = None
+
+    return reversed_tail
